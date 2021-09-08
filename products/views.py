02a21d5e80ec2ee9d0bd4de41a43f67774fb8cc2
ColinHammarberg <colin.hammarberg2@gmail.com>
2021-09-08 12:31:33 +0000

    Working on the product page. Added a slide and some js to it as well.

--- products/views.py
+++ products/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
 from .models import Product
 
-# Products
+# Demonstrating all products available
 
 def all_products(request):
-    # Should show all products loaded
+    """ Products """
 
-    products = Products.objects.all()
-    
-    return render(request, 'products/every_product.html')
+    products = Product.objects.all()
+
+    context = {
+        'products': products,
+    }
+
+    return render(request, 'products/every_product.html', context)
