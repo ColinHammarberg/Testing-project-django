fe2b2c90b940a3d8c84d409ea2f4ded9077be714
ColinHammarberg <colin.hammarberg2@gmail.com>
2021-09-08 11:54:17 +0000

    Working on the products. To be able to get all products.

--- products/views.py
+++ products/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import Product
 
-# Create your views here.
+# Products
+
+def all_products(request):
+    # Should show all products loaded
+
+    products = Products.objects.all()
+    
+    return render(request, 'products/every_product.html')
