6d8de2184783e2839511732df9e2747888cb1f2e
ColinHammarberg <colin.hammarberg2@gmail.com>
2021-09-14 16:48:29 +0000

    Working on the shopping cart page. Required some help from previous lessons.

--- shoppingbag/context.py
+++ shoppingbag/context.py
@@ -1,5 +1,7 @@
 from decimal import Decimal
 from django.conf import settings
+from django.shortcuts import get_object_or_404
+from products.models import Product
 
 # Context processor
 
@@ -8,6 +10,21 @@ def cart_contents(request):
     cart_items = []
     total = 0
     product_count = 0
+    bag = request.session.get('cart', {})
+
+# Required help from previous coding lessons
+
+    for item_id, quantity in bag.items():
+        product = get_object_or_404(Product, pk=item_id)
+        total += quantity * product.price
+        product_count += quantity
+        cart_items.append(
+            {
+                'item_id': item_id,
+                'quantity': quantity,
+                'product': product,
+            }
+        )
 
     delivery = total * Decimal(settings.STANDARD_DELIVERY_THRESHOLD)
     
