9d7d7bcbe905b74ca10e777d6d642207809742cc
ColinHammarberg <colin.hammarberg2@gmail.com>
2021-09-14 09:20:17 +0000

    Began working on contexts.py and delivery.

--- gentlemen/settings.py
+++ gentlemen/settings.py
@@ -77,6 +77,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'shoppingbag.context.cart_contents',
             ],
         },
     },
@@ -162,3 +163,5 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+STANDARD_DELIVERY_THRESHOLD = 20
