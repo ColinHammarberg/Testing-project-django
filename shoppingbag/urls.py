e3a8874e72218a0a508ddb957bde3c2f03773c7e
ColinHammarberg <colin.hammarberg2@gmail.com>
2021-09-17 20:54:31 +0000

    Remove button functionality completed and its path url. Also chnaged some styling areas which I found while testing the site.

--- shoppingbag/urls.py
+++ shoppingbag/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.shoppingbag, name='shoppingbag'),
     path('add/<item_id>/', views.add_product, name='add_product'),
+    path('remove/<item_id>/', views.remove_product, name='remove_product'),
 ]
--- shoppingbag/views.py
+++ shoppingbag/views.py
@@ -1,12 +1,16 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse, HttpResponse
 from django.contrib import messages
+
 from products.models import Product
 
 # Create your views here.
 
+# Shopping cart.
+
 def shoppingbag(request):
     return render(request, 'shoppingbag/shoppingbag.html')
 
+# Adding a product/item to cart.
 
 def add_product(request, item_id):
     """ User can add items to the cart """
@@ -20,12 +24,12 @@ def add_product(request, item_id):
 
     if size:
         if item_id in list(cart.keys()):
-            if size in cart[item_id]['items_by_size'].keys():
-                cart[item_id]['items_by_size'][size] += quantity
+            if size in cart[item_id]['sizes'].keys():
+                cart[item_id]['sizes'][size] += quantity
             else:
-                cart[item_id]['items_by_size'][size] = quantity
+                cart[item_id]['sizes'][size] = quantity
         else:
-            cart[item_id] = {'items_by_size': {size: quantity}}
+            cart[item_id] = {'sizes': {size: quantity}}
     else:
         if item_id in list(cart.keys()):
             cart[item_id] += quantity
@@ -37,5 +41,28 @@ def add_product(request, item_id):
     request.session['cart'] = cart
     return redirect(redirect_url)
 
+# Removing product/item from cart.
+
 def remove_product(request, item_id):
-    """Removing product from shopping cart"""
+    """Remove the selected product/item from the user's/session's cart"""
+
+    try:
+        size = None
+        if 'product_size' in request.POST:
+            size = request.POST['product_size']
+        cart = request.session.get('cart', {})
+
+        if size:
+            del cart[item_id]['sizes'][size]
+            if not cart[item_id]['sizes']:
+                cart.pop(item_id)
+        else:
+            cart.pop(item_id)
+
+        request.session['cart'] = cart
+        return HttpResponse(status=200)
+
+    except Exception as e:
+        return HttpResponse(status=500)
+
+
