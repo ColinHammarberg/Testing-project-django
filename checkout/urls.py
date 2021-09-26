a18cd1d35b47079b3a4c406eabd00d5edcb5ad6d
ColinHammarberg <colin.hammarberg2@gmail.com>
2021-09-26 10:25:47 +0000

    Working on the webhooks from stripe.

--- checkout/urls.py
+++ checkout/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path
 from . import views
+from .webhooks import webhook
 
 urlpatterns = [
     path('', views.checkout, name='checkout'),
     path('success/<order_number>', views.success, name='success'),
+    path('wh/', webhook, name='webhook'),
 
 ]
