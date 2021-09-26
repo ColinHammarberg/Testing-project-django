ee17a6fa8b21da7392976e84b05510e7dea2ac70
ColinHammarberg <colin.hammarberg2@gmail.com>
2021-09-26 09:28:30 +0000

    Continued work on email confirmations. Now I'm moving onto activating and passing the payments through webhooks.

--- checkout/views.py
+++ checkout/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
+from django.core.mail import send_mail
+from django.template.loader import render_to_string
+from django.conf import settings
 
 from .forms import OrderForm
 from .models import Order, OrderLineItem
