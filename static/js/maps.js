e0c8d66e1e835ffe2238e0e8a47d7cc1f325afac
ColinHammarberg <colin.hammarberg2@gmail.com>
2021-10-07 08:31:24 +0000

    Fixed the Google Map API key and organized the folders

--- static/js/maps.js
+++ static/js/maps.js
@@ -2,7 +2,7 @@
     function initMap() {
         map = new google.maps.Map(document.getElementById("map"), {
           center: { lat: 51.509865, lng: -0.118092 },
-          zoom: 9,
+          zoom: 10,
         });
       
       
