4084d712e5bb5f4825aac3815548cd3c23044d0e
ColinHammarberg <colin.hammarberg2@gmail.com>
2022-01-01 20:59:49 +0000

    Making update prodile fields required with blank=flase set in models.

--- gentlemen/settings.py
+++ gentlemen/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 import dj_database_url
+if os.path.exists('env.py'):
+    import env
 
 
 
@@ -27,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['charity-for-gentlemen.herokuapp.com', 'localhost']
 
